6e14ce871208919a6aae4222bd85880b689ba176
bsm amr <bsmamr@gmail.com>
2025-03-19 14:30:35 +0100

    Heberger frontend backend

--- app.js
+++ app.js
@@ -9,13 +9,12 @@ const chatbotRouter=require("./routes/chatbot.route")
 const userRouter = require("./routes/user.route")
 const chatbotRequeteRouter = require("./routes/chatbot-requete.route")
 const paymentRouter =require("./routes/payment.route.js");
+const path = require('path'); // Ajout de l'importation de path
 const app = express ()
 app.use (express.json())
 app.use(cors())
 dotenv.config()
-app.get('/',(req,res)=>{
-    res.send("Bienvenue dans notre site ")
-})
+
 // connexion a la base de données
 mongoose.connect(process.env.DATABASECLOUD)
 
@@ -31,6 +30,12 @@ app.use("/api/chat",chatbotRouter)
 app.use("/api/users",userRouter)
 app.use('/api/chatbot', chatbotRequeteRouter);
 app.use('/api/payment', paymentRouter);
+
+//dist reactjs
+app.use(express.static(path.join(__dirname, './client/build'))); // Route pour les pages non trouvées, redirige vers index.html
+app.get('*', (req, res) => { res.sendFile(path.join(__dirname,'./client/build/index.html')); });
+
+//serveur
 app.listen(4000,()=>{
     console.log(`Serveur is listen on port ${process.env.PORT}`)
 })
